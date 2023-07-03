9fc059e2023bc09504481798f0962050085d77d7
Shaik Abdul Salam <80320404+shaikabdulsalam31@users.noreply.github.com>
2023-07-03 09:59:51 +0530

    Update rails.js

--- RailRoute/rails.js
+++ RailRoute/rails.js
@@ -206,7 +206,7 @@ async function matcher() {
         </head>
         <body>
             <h1>Train Details</h1>
-            <a href="Railroute.html" class="back-link">Back</a>
+            <a href="https://shaikabdulsalam31.github.io/Bus-To-Rail/Railroute.html" class="back-link">Back</a>
       `;
 
       matchingTrains.forEach(train => {
@@ -327,7 +327,7 @@ async function getTrainDetails() {
         </head>
         <body>
             <h1>Train Details</h1>
-            <a href="Railroute.html" class="back-link">Back</a>
+            <a href="https://shaikabdulsalam31.github.io/Bus-To-Rail/Railroute.html" class="back-link">Back</a>
       `;
 
       matchingTrains.forEach(train => {
